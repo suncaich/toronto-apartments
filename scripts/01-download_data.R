fe6c1ca486e2337cf1a367cfb882083c549aff34
suncaich <caichen.sun@mail.utoronto.ca>
2024-09-26 23:12:04 +0800

    Download dataset from Open Data Toronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,6 +1,4 @@
 library(opendatatoronto)
-resource <- list_package_resources("https://open.toronto.ca/dataset/apartment-building-evaluation/")%>%
-  head(1) %>%
-  get_resource()
-write.csv(resource, "data/raw_data/Apartment_Building_Evaluation.csv", row.names = FALSE)
-
+library(dplyr)  
+resource <- list_package_resources("https://open.toronto.ca/dataset/apartment-building-evaluation/") %>% head(1) %>% get_resource()
+write.csv(resource, "../toronto-apartments/data/raw_data/Apartment_Building_Evaluations.csv", row.names = FALSE)
